a27e1a337dcb168b82a465eeaa29fd2c8e59fa37
Jack D <jackdijkema@hotmail.com>
2021-11-02 18:21:39 +0100

    almost finished database upload function

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import pyrebase
+from file import Files
 from pyrebase.pyrebase import Firebase, initialize_app
 
 class Database(object):
@@ -8,3 +9,22 @@ class Database(object):
       firebase = pyrebase.initialize_app(self.config)
       self.db = firebase.database()
 
+
+  def push_csv_to_db(self, array):
+    files = Files()
+    self.array = array
+    vragen = files.get_vragen(self.array)
+    antwoord1 = files.get_antwoord(self.array, 1)
+    teller = 0
+    print(vragen)
+    
+    for (vraag, antwoord) in zip(vragen, antwoord1):
+      print('Vraag:', vraag,'Antwoord:', antwoord, 'Teller:', teller)
+    
+    teller += 1
+    
+      # self.db.child(teller).child(i).set(vraag, antwoord)
+    
+    
+
+          
