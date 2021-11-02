752c57792c2cd1f1e62841f301bcfa2c3ded3237
StanSmits <smitsstan2004@gmail.com>
2021-11-02 13:18:13 +0100

    Verandering aan config

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ file = Files()
 csv = 'Vragen2.csv'
 
 firebase_config = {
-"apiKey": "AIzaSyDOgtw11XJT7X3Kv4JJG96pVL0d_F7tYws",
-"authDomain": "challengeweek-6a91e.firebaseapp.com",
-"databaseURL": "https://challengeweek-6a91e-default-rtdb.europe-west1.firebasedatabase.app",
-"projectId": "challengeweek-6a91e",
-"storageBucket": "challengeweek-6a91e.appspot.com",
-"messagingSenderId": "1012250981140",
-"appId": "1:1012250981140:web:8f4fc6f9419ec9acdec801"
+    "apiKey": "AIzaSyDOgtw11XJT7X3Kv4JJG96pVL0d_F7tYws",
+    "authDomain": "challengeweek-6a91e.firebaseapp.com",
+    "databaseURL": "https://challengeweek-6a91e-default-rtdb.europe-west1.firebasedatabase.app",
+    "projectId": "challengeweek-6a91e",
+    "storageBucket": "challengeweek-6a91e.appspot.com",
+    "messagingSenderId": "1012250981140",
+    "appId": "1:1012250981140:web:8f4fc6f9419ec9acdec801"
 }
 
 db.connection_database(firebase_config)
