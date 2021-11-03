a3f7bb115ce02ea80c8c10341e0dcf79dcc4bce6
stijnpotters1 <93516547+stijnpotters1@users.noreply.github.com>
2021-11-03 15:42:46 +0100

    Update database.py

--- database.py
+++ database.py
@@ -2,6 +2,9 @@ import pyrebase
 from file import Files
 from pyrebase.pyrebase import Firebase, initialize_app
 
+files = Files()
+
+
 class Database(object):
 
   def connection_database(self, config):
@@ -11,20 +14,31 @@ class Database(object):
 
 
   def push_csv_to_db(self, array):
-    files = Files()
     self.array = array
     vragen = files.get_vragen(self.array)
-    antwoord1 = files.get_antwoord(self.array, 1)
+    # antwoorden = files.get_antwoord(self.array, 1)
+    # afstudeer_richting = files.get_antwoord(self.array, 1)
+    # print(antwoorden)
     teller = 0
-    print(vragen)
-    
-    for (vraag, antwoord) in zip(vragen, antwoord1):
-      print('Vraag:', vraag,'Antwoord:', antwoord, 'Teller:', teller)
-    
-    teller += 1
-    
-      # self.db.child(teller).child(i).set(vraag, antwoord)
-    
-    
 
-          
+    data = []
+    
+    for vraag_nummer in range(len(vragen)):
+      data_vraag = {
+        "vraag": vragen[vraag_nummer],
+        "antwoorden": files.get_antwoord(vraag_nummer),
+        "richting": files.get_specialiteit(array)
+      }
+
+      data.append(data_vraag)
+
+    print(data)
+    self.db.set(data)
+
+    # for (vraag, antwoord, richting) in zip(vragen, antwoorden, afstudeer_richting):
+      # if None: return
+      #data = {"Vraag": vraag, "Antwoord": antwoord[0], "Antwoord2": antwoord[1], "Antwoord3": antwoord[2]}
+      # self.db.child(teller).set(data)
+     # teller += 1
+      
+    #print('Vraag:', vraag, 'Antwoord:', antwoord, "Richting", richting, 'Teller:', teller)
