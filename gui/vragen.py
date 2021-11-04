1f0baa247b81b3d570b1d6b10179918f542f153a
stijnpotters1 <93516547+stijnpotters1@users.noreply.github.com>
2021-11-04 11:50:20 +0100

    betere versie

--- gui/vragen.py
+++ gui/vragen.py
@@ -1,19 +1,18 @@
 import PySimpleGUI as simple_gui
 
-main_menu_layout = [[simple_gui.Image("imageedit_1_4801288964.png", size=(900, 600))],
-                    [simple_gui.Button("Start", size=(0, 2), expand_x=True),simple_gui.Button("Start", size=(0, 2), expand_x=True)],
-                    [simple_gui.Button("Start", size=(0, 2), expand_x=True),simple_gui.Button("Start", size=(0, 2), expand_x=True)]
-                    ]
-                    
+main_menu_layout = [[simple_gui.Text('Vraag 1', font='Courier 15', text_color='white', background_color='#283b5b', size=(75,2),justification='center')],
+                    [simple_gui.Image("imageedit_1_4801288964.png", size=(900, 550))],
+                    [simple_gui.Button("antwoord A", size=(0, 2), expand_x=True),simple_gui.Button("antwoord B", size=(0, 2), expand_x=True)],
+                    [simple_gui.Button("antwoord C", size=(0, 2), expand_x=True),simple_gui.Button("antwoord D", size=(0, 2), expand_x=True)]
+                    ]  
 
-main_menu = simple_gui.Window(title='Codefuse Site',
+main_menu = simple_gui.Window(title='Vraag 1',
                               layout=main_menu_layout,
+                              background_color="black",
                               size=(900, 700),
-                              element_justification="c", finalize=True, margins=(0,0), element_padding=(2,2)
+                              element_justification="c", finalize=True, margins=(1,0), element_padding=(2,2)
                             )
                               
-
-
 while True:
     event, values = main_menu.read()
     if event == simple_gui.WIN_CLOSED or event == 'Cancel':
