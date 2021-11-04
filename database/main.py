f35e5d6c74412364390e3cb792f160e535c7f4eb
Jack <jackdijkema@hotmail.com>
2021-11-04 22:03:44 +0100

    config change

--- database/main.py
+++ database/main.py
@@ -4,28 +4,24 @@ from database import Database
 from file import Files
 import pyrebase
 
-# From firebase.env, import all the data and put it into the variable firebase_config
-load_dotenv()
 firebase_config = {
-    "apiKey": os.getenv("API_KEY"),
-    "authDomain": os.getenv("AUTH_DOMAIN"),
-    "databaseURL": os.getenv("DATABASE_URL"),
-    "projectId": os.getenv("PROJECT_ID"),
-    "storageBucket": os.getenv("STORAGE_BUCKET"),
-    "messagingSenderId": os.getenv("MESSAGING_SENDER_ID"),
-    "appId": os.getenv("APP_ID"),
-    "measurementId": os.getenv("MEASUREMENT_ID")
-}
+   "apiKey": "AIzaSyDOgtw11XJT7X3Kv4JJG96pVL0d_F7tYws",
+   "authDomain": "challengeweek-6a91e.firebaseapp.com",
+   "databaseURL": "https://challengeweek-6a91e-default-rtdb.europe-west1.firebasedatabase.app",
+   "projectId": "challengeweek-6a91e",
+   "storageBucket": "challengeweek-6a91e.appspot.com",
+   "messagingSenderId": "1012250981140",
+   "appId": "1:1012250981140:web:8f4fc6f9419ec9acdec801",
+   "DEFAULT_TOKEN": "7igGn3usI1vfEr7uItZdLcdqr12mz5iDviu3HBRt"
+ }
 
 db = Database()
 file_i = Files()
 
 csv = 'Vragen.csv'
 
-db.connection_database(firebase_config)
-
 file_array = file_i.csv_to_array(csv)
 
+db.connection_database(firebase_config)
 
-print(db.push_csv_to_db(file_array))
-
+# print(db.push_csv_to_db(file_array))
