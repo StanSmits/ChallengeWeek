c86c47e82d19eb58924f143cafd2c29a6c638a4c
Jack <jackdijkema@hotmail.com>
2021-11-04 13:42:20 +0100

    afweging functie

--- database/file.py
+++ database/file.py
@@ -7,6 +7,7 @@ class Files(object):
             reader = csv.reader(f)
             return list(reader)
 
+    # Krijg alle vragen uit de array
 
     def get_vragen(self, array):
         self.array = array
@@ -14,10 +15,16 @@ class Files(object):
         for i in self.array:
             return_array.append(i[0])
         return return_array
-            
-            
+
     def get_antwoord(self, vraag_nummer):
         return self.array[vraag_nummer][1].split(";")
+    
+    
+    def get_afweging(self, vraag_nummer):
+        # Get the 3rd indice of the array, from that get the key "afweging", split all the values with the key ";" and return them
+        return self.array[vraag_nummer][3].split(";")
+
+
         
     
     def get_specialiteit(self, array):
